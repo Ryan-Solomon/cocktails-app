e10e99b124f19ffc40cce7c527a38c207283fb2a
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-11-07 21:04:13 -0600

    adds about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './styles/app.scss';
 import Home from './pages/Home';
+import About from './pages/About';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/about'>
+            <About />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
---
+++ src/pages/About.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <main className='container'>
+      <h1>About Us</h1>
+      <div className='content'>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod eius
+          omnis expedita natus, vitae, esse accusantium recusandae assumenda
+          nemo blanditiis unde doloribus, nihil impedit possimus? Vero fugiat
+          quo quos dolor. Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Quod eius omnis expedita natus, vitae, esse accusantium
+          recusandae assumenda nemo blanditiis unde doloribus, nihil impedit
+          possimus? Vero fugiat quo quos dolor. Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Quod eius omnis expedita natus, vitae,
+          esse accusantium recusandae assumenda nemo blanditiis unde doloribus,
+          nihil impedit possimus? Vero fugiat quo quos dolor. Lorem, ipsum dolor
+          sit amet consectetur adipisicing elit. Quod eius omnis expedita natus,
+          vitae, esse accusantium recusandae assumenda nemo blanditiis unde
+          doloribus, nihil impedit possimus? Vero fugiat quo quos dolor.
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default About;
