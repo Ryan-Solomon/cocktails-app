259b24319e953a7554667bc0694c7385e768628a
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-11-07 20:46:36 -0600

    adds cocktails

---
+++ src/components/Cocktails.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Cocktails = () => {
+  return (
+    <div className='cocktails-container'>
+      <div className='cocktails__header'>
+        <h1>Cocktails</h1>
+      </div>
+      <div className='cocktails__cards'>
+        <div>Card 1</div>
+        <div>Card 2</div>
+        <div>Card 3</div>
+        <div>Card 4</div>
+        <div>Card 5</div>
+        <div>Card 6</div>
+      </div>
+    </div>
+  );
+};
+
+export default Cocktails;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './../components/Nav';
 import Search from './../components/Search';
+import Cocktails from './../components/Cocktails';
 
 const Home = () => {
   return (
     <div className='home-page-container'>
       <Nav />
       <Search />
+      <Cocktails />
     </div>
   );
 };
